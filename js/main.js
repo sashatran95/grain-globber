b6be293e20c00a2135b38f458e77a822650ea5bf
Sashtastic <ht.sasha.tran@gmail.om>
2016-03-18 14:44:00 -0400

    will it work?...

---
+++ js/main.js
@@ -0,0 +1,15 @@
+grains.forEach(function (item) {
+  var $li = $('<li>');
+  var $ul = $('ul');
+  var $p = $('<p>');
+  var $img = $('<img>');
+  var $h2 = $('<h2>');
+
+  $ul.append($li);
+  $img.attr('src', 'images/' + item.img);
+  $li.append($img);
+  $h2.html(item.name);
+  $li.append($h2);
+  $p.html(item.desc);
+  $li.append($p);
+});
